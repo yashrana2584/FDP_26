78737df56c7e41443c64c3d896a5089e4d244c6e
Yash <ranayash2584@gmail.com>
2026-03-13 10:54:44 +0530

    exp6

---
+++ myproject/accounts/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+
+def home(request):
+    return render(request, "accounts/home.html")
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = UserCreationForm()
+    return render(request, "accounts/register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = AuthenticationForm()
+    return render(request, "accounts/login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+@login_required
+def profile(request):
+    return render(request, "accounts/profile.html")
